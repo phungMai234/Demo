be014ee9434650cd65149049a87d1c576ed3e7d6
phungMai234 <37433566+phungMai234@users.noreply.github.com>
2018-03-28 09:30:26 +0700

    demo1.0

---
+++ 17021389.th03.bt11 (1).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    vector<long> a;
+
+    long x, count = 0;
+    float sum = 0, u = 0;
+    while(cin >> x)
+    {
+        a.push_back(x);
+
+        /*if(x < -100)
+        {
+            break;
+        }*/
+        sum += x;
+        count++;
+
+
+    }
+    sum /= count;
+    long i;
+    long taget;
+    for(i = 0 ; i < count; i++)
+    {
+
+        u += (a[i] - sum) * (a[i] - sum);
+
+    }
+    u /= (count );
+
+    cout << fixed << setprecision(2) << sum << "\n" << u;
+
+}
